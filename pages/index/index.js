25c7cfa6839a83d19901a96fbd07e24b1fd97e44
章鱼丸 <kuailemario@gmail.com>
2018-03-23 16:09:02 +0800

    添加时间

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,7 +14,12 @@ Page({
     motto: '第二周任务，完成“我的”选项卡',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    year:'',
+    month:'',
+    day: '',
+    date: 0,
+    week:['日','一','二','三','四','五','六','日']
   },
   //事件处理函数
   bindViewTap: function() {
@@ -24,37 +29,41 @@ Page({
   },
   
   onLoad: function () {
-    /**if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }**/
+    //获取当前时间戳  
+    var timestamp = Date.parse(new Date());
+    timestamp = timestamp / 1000;
+    console.log("当前时间戳为：" + timestamp);
+
+    //获取当前时间  
+    var n = timestamp * 1000;
+    var date = new Date(n);
+    //年  
+    var Y = date.getFullYear();
+    //月  
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+    //日  
+    var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    //时  
+    var h = date.getHours();
+    //分  
+    var m = date.getMinutes();
+    //秒  
+    var s = date.getSeconds();
+    //星期
+    var w = date.getDay();
+
+    this.setData({
+      year:Y,
+      month:M,
+      day: D,
+      date: w,
+    });
+
     // 调用函数时，传入new Date()参数，返回值是日期和时间  
     var time = util.formatTime(new Date());
     // 再通过setData更改Page()里面的data，动态更新页面的数据  
     this.setData({
-      time: time
+      time: time,
     });
   },
   getUserInfo: function(e) {
